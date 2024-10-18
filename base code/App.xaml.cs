64a7bcc474f9d37293baa55eb0a12d86111516d9
dwu02 <d.s.wu@wustl.edu>
2024-10-18 13:23:53 -0500

    boiler code

---
+++ base code/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace base_code
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ base code/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace base_code
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
